bb6985e245d12ced864621863c211603f512609f
suming9 <suming9@hikvision.com.cn>
2024-09-23 16:56:23 +0800

    feat：第一次错误提交revert

--- src/main/java/com/franksu/springbootrabbitmq/juc/TestGit.java
+++ src/main/java/com/franksu/springbootrabbitmq/juc/TestGit.java
@@ -11,6 +11,6 @@ package com.franksu.springbootrabbitmq.juc;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Starting");
-        System.out.println("第一次错误提交");
+        System.out.println("第一次错误提交revert");
     }
 }
