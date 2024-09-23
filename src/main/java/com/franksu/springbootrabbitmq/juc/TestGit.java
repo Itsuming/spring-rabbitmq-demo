b75f16ab88678b694a57312498a7b451a9af6d19
suming9 <suming9@hikvision.com.cn>
2024-09-23 15:58:41 +0800

    feat：测试git代码提交

---
+++ src/main/java/com/franksu/springbootrabbitmq/juc/TestGit.java
@@ -0,0 +1,15 @@
+package com.franksu.springbootrabbitmq.juc;
+
+/**
+ * @BelongsProject: spring-rabbitmq
+ * @BelongsPackage: com.franksu.springbootrabbitmq.juc
+ * @Author: suming9
+ * @CreateTime: 2024-09-23  15:57
+ * @Description: 测试git操作
+ * @Version: 1.0
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Starting");
+    }
+}
